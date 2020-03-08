088f4c469f5d76587c1a7d449f281a2987c38405
Adrian González Pardo <gozapaadr@gmail.com>
2020-03-08 14:26:11 -0600

    Adding the second repository for working

--- installArchScript.sh
+++ installArchScript.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
 
-echo "Cambio de distribución de teclado a español latino"
+#echo "Cambio de distribución de teclado a español latino"
 loadkeys la-latin1
 echo "Actualización del sistema de archivos y repositorios"
 pacman -Sy
 
+git clone https://github.com/helmuthdu/aui.git
 
+cd aui
+echo "Ejecucion para seleccion de distribución de teclado y otras cosas"
+sleep 1
+./fifo
